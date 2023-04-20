bad6180cc40c6954dd73c354ee9c89c5fed840a6
yuniyolli <yuniyolli@gmail.com>
2023-04-20 09:54:02 +0900

    feat: ScannerText 추가

---
+++ src/main/java/com/example/javaexercise/ScannerText.java
@@ -0,0 +1,12 @@
+package com.example.javaexercise;
+
+import java.util.Scanner;
+
+public class ScannerText {
+    public static void readTwoWords() {
+        Scanner sctxt = new Scanner(System.in);
+
+        System.out.println(sctxt.next() + sctxt.next());
+
+    }
+}
---
+++ src/main/java/com/example/javaexercise/ScannerTextTest.java
@@ -0,0 +1,8 @@
+package com.example.javaexercise;
+
+public class ScannerTextTest {
+    public static void main(String[] args) {
+        ScannerText scannertext = new ScannerText();
+        ScannerText.readTwoWords();
+    }
+}
