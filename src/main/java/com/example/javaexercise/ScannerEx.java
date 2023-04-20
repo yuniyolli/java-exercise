8379045ba3e6266940099023216b21a179654b8c
yuniyolli <yuniyolli@gmail.com>
2023-04-20 09:23:22 +0900

    feat: Scanner 연습추가

---
+++ src/main/java/com/example/javaexercise/ScannerEx.java
@@ -0,0 +1,12 @@
+package com.example.javaexercise;
+
+import java.util.Scanner;
+
+public class ScannerEx {
+    public void readTwoNumbersAndPlus() {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println(sc.nextInt() + sc.nextInt());
+    }
+
+}
---
+++ src/main/java/com/example/javaexercise/ScannerExTest.java
@@ -0,0 +1,13 @@
+package com.example.javaexercise;
+
+import java.util.Scanner;
+
+public class ScannerExTest {
+    public static void main(String[] args) {
+
+            ScannerEx scannerEx = new ScannerEx();
+            scannerEx.readTwoNumbersAndPlus();
+
+
+    }
+}
