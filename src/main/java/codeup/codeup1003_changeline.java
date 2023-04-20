7ff0d84d47f36154e09656092074cb7ac1e6637f
yuniyolli <yuniyolli@gmail.com>
2023-04-20 21:03:09 +0900

    feat: codeup 01~04 추가

---
+++ src/main/java/codeup/codeup1003_changeline.java
@@ -0,0 +1,13 @@
+package codeup;
+
+public class codeup1003_changeline {
+
+    /*
+     /n
+    System.getProperty("line.separator")
+    System.lineSeparator()
+    */
+    public static void main(String[] args) {
+        System.out.println("Hello\nWorld");
+    }
+}
