cbaaab40aca01f5f905feb9398056fe5544d24b7
yuniyolli <yuniyolli@gmail.com>
2023-04-20 20:59:42 +0900

    feat: Scanner관련 파일추가 1. ScannerTextAdvice 2. ScannerTextAdviceTest

---
+++ src/main/java/com/example/javaexercise/ScannerTextAdviceTest.java
@@ -0,0 +1,14 @@
+package com.example.javaexercise;
+
+import java.util.Scanner;
+
+public class  ScannerTextAdviceTest {
+    public static void main(String[] args) {
+        ScannerTextAdvice scannerTextAdvice = new ScannerTextAdvice();
+        scannerTextAdvice.inputTwoNumbersAndPlus();
+
+    }
+
+
+}
+
