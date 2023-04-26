a6b4b339dc4a8428a6f84ac9e83da48be8e8522b
yuniyolli <yuniyolli@gmail.com>
2023-04-26 09:25:37 +0900

    feat: stringsplit, arr 추가

---
+++ src/main/java/week2day2/FloatStringArr.java
@@ -0,0 +1,12 @@
+package week2day2;
+
+import java.util.Arrays;
+
+public class FloatStringArr {
+    public static void main(String[] args) {
+        float[] fArr = new float[3];
+        String[] sArr = new String[10];
+        System.out.println(Arrays.toString(fArr));
+        System.out.println(Arrays.toString(sArr));
+    }
+}
