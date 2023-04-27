870b12de8819d56f8b10cf4b3f7b57dbd3c2cf86
yuniyolli <yuniyolli@gmail.com>
2023-04-27 19:49:44 +0900

    feat: week2day3 내용추가 - BankAccount

--- src/main/java/week2day3/BankAccount.java
+++ src/main/java/week2day3/BankAccount.java
@@ -1,5 +1,15 @@
 package week2day3;
 
 public class BankAccount {
-    //178페이지 아래
+    public static void main(String[] args) {
+
+
+        //178페이지 아래
+        int myAccount = 1_000_000;
+        myAccount = myAccount - 500_000;
+        System.out.println(myAccount);
+
+        myAccount = myAccount + 4_000_000;
+        System.out.println(myAccount);
+    }
 }
