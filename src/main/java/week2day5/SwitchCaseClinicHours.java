e0ac2ca72a2ad81a3f90c4f9fd4f8459fd13e516
yuniyolli <yuniyolli@gmail.com>
2023-04-28 13:40:29 +0900

    feat: SwitchCaseClinicHours 추가

---
+++ src/main/java/week2day5/SwitchCaseClinicHours.java
@@ -0,0 +1,26 @@
+package week2day5;
+
+import java.util.Scanner;
+
+public class SwitchCaseClinicHours {
+    public static void main(String[] args) {
+        System.out.println("진료 일정이 궁금한 요일을 월/화/수/목/금/토/일 로 입력하세요.");
+
+        Scanner sc = new Scanner(System.in);
+        String day = sc.next();
+        String time = "휴진";
+
+        switch(day) {
+            case "월", "화", "목", "금" :
+                time = "09:30-18:30 "; break;
+            case "토" :
+                time = "09:30-13:00; " ; break;
+            case "수", "일" :
+                time = "휴진"; break;
+
+
+        }
+        System.out.printf("%s요일 진료시간은 %s입니다.", day, time);
+
+    }
+}
