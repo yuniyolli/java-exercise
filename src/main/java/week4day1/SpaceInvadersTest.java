9a88561036448deac82f3d3d3433cefc95ae3a9e
yuniyolli <yuniyolli@gmail.com>
2023-05-08 11:18:46 +0900

    -feat: SpaceInvaders and SpaceInvadersTest added

---
+++ src/main/java/week4day1/SpaceInvadersTest.java
@@ -0,0 +1,15 @@
+package week4day1;
+
+public class SpaceInvadersTest {
+    public static void main(String[] args) {
+    SpaceInvaders si = new SpaceInvaders();
+    si.moveLeft();
+        System.out.println(si.location);
+    si.moveLeft();
+        System.out.println(si.location);
+        si.moveLeft();
+        System.out.println(si.location);
+        si.moveRight();
+        System.out.println(si.location);
+    }
+    }
